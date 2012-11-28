2813bdb662a389b31b2b7b5b82acd92a095f33dc
Jeff Rose <jeff@deepmind.com>
2012-11-28 16:39:30 +0000

    checking whether a table has any data to determine whether it's a seq or map.

--- fn/seq.lua
+++ fn/seq.lua
@@ -8,7 +8,7 @@ seq = {}
 -- A boolean predicate returning true if s can be treated
 -- as a sequence (i.e. an iterator function, table, or tensor)
 function seq.is_seq(s)
-    return util.is_fn(s) or util.is_table(s)
+    return util.is_fn(s) or (util.is_table(s) and #s > 0)
 end
 
 
